696680a6192a07e2cd1266324ea0b654603e0b22
Kayla Ondracek and Nico Daunt <kayla.renee@gmail.com and nico.daunt@gmail.com>
2018-04-24 09:43:24 -0700

    add another passing test for CalcWeight

--- SpaceE.Tests/ModelTests/FuelWeightTests.cs
+++ SpaceE.Tests/ModelTests/FuelWeightTests.cs
@@ -20,5 +20,19 @@ namespace SpaceE.Tests
             //Assert
             Assert.AreEqual(595, intResult);
         }
+        
+        [TestMethod]
+        public void GetTotalFuel_ReturnsFuelWeight2_Int()
+        {
+            //Arrange
+            var weight = new FuelWeight();
+
+            //Act
+            var result = Math.Round((double)weight.CalcWeight(2000, 9400));
+            int intResult = (int)result;
+
+            //Assert
+            Assert.AreEqual(43886, intResult);
+        }
     }
 }
