aa2cc2a454e4fe3bf07093c43ef23f2d3f9301d6
Kayla Ondracek and Nico Daunt <kayla.renee@gmail.com and nico.daunt@gmail.com>
2018-04-24 09:35:54 -0700

    add passing test for CalcWeight

--- SpaceE/Models/FuelWeight.cs
+++ SpaceE/Models/FuelWeight.cs
@@ -9,7 +9,10 @@ namespace SpaceE.Models
     {
         public int CalcWeight(int weight, int distance)
         {
-            return 5;
+            double power = distance / 3000D;
+            double total = Math.Round(weight * Math.Pow(2.718, power)) - weight;
+            int intTotal = (int)total;
+            return intTotal;
         }
 
     }
